7f1e7d2b2d6fa5cd48e50a020c144e7e714f3b49
William D. Yurkow <bcwill42@gmail.com>
2019-10-22 19:44:55 -0400

    creating the trainer page

---
+++ client/src/pages/Trainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import App from '/src/App.js';
+import API from "../utils/API";
+import "./style.css";
+
+
+class Trainer extends Component {
+  state = {
+    lessons: [],
+    synopsis: ""
+  };
+
+
+  componentDidMount() {
+
+  }
+
+
+  export default Trainer;
+  
+
