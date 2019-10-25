eb27d325536a56ae883e157ee5f98569cd515b48
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-10-24 21:48:23 -0400

    changed everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import Nav from "./components/Nav";
+import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from './components/NotFound/NotFound';
+
+import Home from './components/Home/Home';
+
+import HelloWorld from './components/HelloWorld/HelloWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/helloworld" component={HelloWorld} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
