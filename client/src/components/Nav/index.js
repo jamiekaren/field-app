dbb37e17e0e9d274f9ff8fc543a9d0892fa2d254
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-10-29 19:35:28 -0400

    added logo

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../media/Amman-Imman-Logo-mobile.png"
 
 function Nav() {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-dark">
-      <Link className="navbar-brand" to="/">
+      {/* <Link className="navbar-brand" to="/">
         Field App
-      </Link>
+      </Link> */}
       <Link className="navbar-brand ml-auto mr-5" to="/login">
         Login
       </Link>
     </nav>
     <header className="nav2 mb-5 py-5 sticky-top mw-100%">
-
+     <a href="/"><img src={logo} className="ml-4" ></img> </a> 
     </header>
     </>
   )
