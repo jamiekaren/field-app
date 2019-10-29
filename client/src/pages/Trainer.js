d1f830f0907b7b43f25e92ee741828cb0c5a0119
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-29 19:42:54 -0400

    Created new branch from master so it's up to date.

---
+++ client/src/pages/Trainer.js
@@ -0,0 +1,78 @@
+// import dependences and data
+import React, { Component } from "react";
+import Media from "../media/media.json";
+import WashHands from "../media/wash-hands.jpg";
+// import API from "./src/utils/API.js";
+class Trainer extends Component {
+  state = {
+    Meda: [...Media],
+    currentLesson: false
+  };
+  // componentDidMount() {
+  //   // this.loadCurrentLesson();
+  // }
+  handleHardClick = image => {
+    console.log()
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-success class d-flex justify-space-between">
+          <span className="navbar-brand mb-0">Field App PRO</span>
+        </nav>
+        <div className="jumbotron jumbotron-fluid bg-primary text-white text-center">
+          <h1>Welcome Trainer!</h1>
+        </div>
+        <div className="container-fluid">
+          <element className="groupLesson post">
+            <card>
+              <img
+                onClick=""
+                className="imageHand"
+                src={WashHands}
+                alt="WashingHands"
+                style={{
+                  width: 200,
+                  height: 200,
+                  position: "right",
+                  bottom: this.props.bottom
+                }}
+              />
+            </card>
+            <card>
+              <img
+                onClick=""
+                className="imageHand"
+                src={WashHands}
+                alt="WashingHands"
+                style={{
+                  width: 200,
+                  height: 200,
+                  position: "left",
+                  bottom: this.props.bottom
+                }}
+              />
+            </card>
+            <card>
+              <img
+                onClick=""
+                className="imageHand"
+                src={WashHands}
+                alt="WashingHands"
+                style={{
+                  width: 200,
+                  height: 200,
+                  position: "relative",
+                  bottom: this.props.bottom
+                }}
+              />
+            </card>
+          </element>
+          <div className="container home row align-items-center justify-content-between"></div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+// export Clicky component
+export default Trainer;
