d3e7053bc1450d301a9a83fed7de93f35a81e7bf
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-31 12:52:54 -0400

    UPDATES: TESTING SLIDER FOR SMART COMPONENT ABILITY - Sucessfully got it working in a smart component - Able to udpate images using state in this component, ideally we'd be sending in props

--- client/src/pages/Lessons/Lessons.js
+++ client/src/pages/Lessons/Lessons.js
@@ -1,86 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Slide } from 'react-slideshow-image';
 import Wash from '../../media/wash-hands.jpg';
 import Brush from '../../media/brush11.jpg';
 import Brush2 from '../../media/brush12.jpg';
-import ('./style.css');
-// import React from 'react';
+import('./style.css');
 
 
-// class Lessons extends Component {
 
-//     render() {
-//         const slideImages = [
-//             'client\src\media\brush11.jpg',
-//             'client\src\media\brush12.jpg',
-//             'client\src\media\wash-hands.jpg'
-//         ];
-
-//         const properties = {
-//             duration: 5000,
-//             transitionDuration: 500,
-//             infinite: true,
-//             indicators: true,
-//             arrows: true,
-//             onChange: (oldIndex, newIndex) => {
-//                 console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-//             }
-//         }
-
-//         const Slideshow = () => {
-//             console.log(slideImages[0])
-//             return (
-//                 <div className="slide-container">
-//                     <Slide {...properties}>
-//                         <div className="each-slide">
-//                             <div style={{ 'backgroundImage': `url(${slideImages[0]})` }}>
-//                                 <span>Slide 1</span>
-//                             </div>
-//                         </div>
-//                         <div className="each-slide">
-//                             <div style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
-//                                 <span>Slide 2</span>
-//                             </div>
-//                         </div>
-//                         <div className="each-slide">
-//                             <div style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
-//                                 <span>Slide 3</span>
-//                             </div>
-//                         </div>
-//                     </Slide>
-//                 </div>
-//             )
-//         }
-//     }
-// }
-
-// export default Lessons
-// class Lessons extends Component {
-    const slideImages = [
-        '../../media/brush11.jpg',
-        '../../media/brush12.jpg',
-        '../../media/wash-hands.jpg'
-    ];
-
-    const properties = {
-        duration: 5000,
-        transitionDuration: 500,
-        infinite: true,
-        indicators: true,
-        arrows: true,
-        autoplay: false,
-        onChange: (oldIndex, newIndex) => {
-            console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-        }
+class Lessons extends Component {
+    state = {
+        brush11: Brush,
+        brush12: Brush2,
+        wash: Wash
     }
 
-    const Slideshow = () => {
-        console.log(slideImages);
+    // const slideImages = [
+    //     '../../media/brush11.jpg',
+    //     '../../media/brush12.jpg',
+    //     '../../media/wash-hands.jpg'
+    // ];
+
+
+
+
+    // const Slideshow = () => {
+
+
+    render() {
+
+        
+        const properties = {
+            duration: 5000,
+            transitionDuration: 500,
+            infinite: true,
+            indicators: true,
+            arrows: true,
+            autoplay: false,
+            onChange: (oldIndex, newIndex) => {
+                console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+            }
+        }
+
+
         return (
             <div className="slide-container">
                 <Slide {...properties}>
                     <div className="each-slide">
-                        <div style={{ 'backgroundImage': `url(${Wash})` }}>
+                        <div style={{ 'backgroundImage': `url(${this.state.brush11})` }}>
                             <span>Slide 1</span>
                         </div>
                     </div>
@@ -99,4 +65,6 @@ import ('./style.css');
         )
     }
 
-export default Slideshow;
+}
+
+export default Lessons;
