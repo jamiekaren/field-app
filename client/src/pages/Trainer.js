f1726ea8dada8e6d8c18dbeceedd9c8c1561529e
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-11-05 19:14:51 -0500

    Stuff

--- client/src/pages/Trainer.js
+++ client/src/pages/Trainer.js
@@ -1,104 +1,134 @@
 // import dependences and data
 import React, { Component } from "react";
-import Media from "../media/media.json";
-import WashHands from "../media/lesson2/wash-hands.jpg";
-// import Lessons from "./Lessons/Lessons";
-import Slideshow from "../components/Slideshow/index.js";
+import { Slide } from 'react-slideshow-image';
+import WashHands1 from '../media/lesson1/brush11.jpg';
+import WashHands2 from '../media/lesson1/brush12.jpg';
+import WashHands3 from '../media/lesson1/brush13.jpg';
 
-// import API from "./src/utils/API.js";
+
+
+// const lessonOne = [Brush, Brush2];
+const lessonTwo = [WashHands1, WashHands2, WashHands3];
+// const lessonThree = [Brush, Brush2];
 
 
 class Trainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Media: Media[0],
-      currentLesson: "",
-      Wash: WashHands,
+      currentLesson: [...lessonTwo],
+      allLessons: [lessonTwo],
       showComponent: false,
-      showing: true,
-      lessonArray: ["../../media/lesson1/brush11.jpg", "../../media/lesson1/brush12.jpg"]
+      properties: {
+        duration: 5000,
+        transitionDuration: 500,
+        infinite: true,
+        indicators: true,
+        arrows: true,
+        autoplay: false,
+        onChange: (oldIndex, newIndex) => {
+          console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+        }
+      }
     };
     this._onButtonClick = this._onButtonClick.bind(this);
   }
 
-  // componentDidMount() {
-  //   // this.loadCurrentLesson();
+  // renderSlideshow() {
+  //   <div className="slide-container">
+  //     <Slide {...this.state.properties}>
+  //       {this.state.currentLesson.map(image =>
+  //         <div key="" className="each-slide">
+  //           <div style={{ 'backgroundImage': `url(${image})` }}>
+  //           </div>
+  //         </div>
+  //       )}
+
+  //     </Slide>
+  //   </div>
   // }
 
-  //  componentDidMount(){
-  //    console.log("Our json", Media);
-  //  };
-  renderSlideshow() {
-    
-  }
-
   _onButtonClick() {
+
+    
+
     this.setState({
       showComponent: true,
     });
   }
 
-  render() {
-    const { showing } = this.state;
+  render = () => {
+     
     return (
       <React.Fragment>
 
         <div className="container-fluid">
           <element className="groupLesson post">
-          {this.state.showComponent  ?
-          
-         this.renderSlideshow()
-               :
-               <div>
+            {this.state.showComponent ?
+
+              <div className="slide-container">
+                <Slide {...this.state.properties}>
+                  {this.state.currentLesson.map(image =>
+                    <div key="" className="each-slide">
+                      <div style={{ 'backgroundImage': `url(${image})` }}>
+                      </div>
+                    </div>
+                  )}
+
+                </Slide>
+              </div>
+
+              // this.renderSlideshow()
+              :
+              <div>
                 <card>
-               <img
-               onClick={this._onButtonClick}
-               className="imageHand"
+                  <img
+                    onClick={this._onButtonClick}
+                    className="imageHand"
 
-               src={WashHands}
-               alt="WashingHands"
-               style={{
-                 width: 200,
-                 height: 200,
-                 position: "right",
-                 bottom: this.props.bottom
-               }}
-             />
-            
-            </card>
+                    src={WashHands1}
+                    alt="WashingHands"
+                    style={{
+                      width: 200,
+                      height: 200,
+                      position: "right",
+                      bottom: this.props.bottom
+                    }}
+                  />
 
-            <card>
-              <img
-                onClick=""
-                className="imageHand"
-                src={WashHands}
-                alt="WashingHands"
-                style={{
-                  width: 200,
-                  height: 200,
-                  position: "left",
-                  bottom: this.props.bottom
-                }}
-              />
-            </card>
+                </card>
 
-            <card>
-              <img
-                onClick=""
-                className="imageHand"
-                src={WashHands}
-                alt="WashingHands"
-                style={{
-                  width: 200,
-                  height: 200,
-                  position: "relative",
-                  bottom: this.props.bottom
-                }}
-              />
-            </card>
-            </div>
-              }
+                <card>
+                  <img
+                    onClick=""
+                    className="imageHand"
+                    src={WashHands2}
+                    alt="WashingHands"
+                    style={{
+                      width: 200,
+                      height: 200,
+                      position: "left",
+                      bottom: this.props.bottom
+                    }}
+                  />
+                </card>
+
+                <card>
+                  <img
+                    onClick=""
+                    className="imageHand"
+                    src={WashHands3}
+                    alt="WashingHands"
+                    style={{
+                      width: 200,
+                      height: 200,
+                      position: "relative",
+                      bottom: this.props.bottom
+                    }}
+                  />
+                </card>
+              </div>
+            }
 
           </element>
           <div className="container home row align-items-center justify-content-between"></div>
